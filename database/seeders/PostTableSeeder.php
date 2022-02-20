8f9052f8cb6476d7ba2eb067f40d09e0dd436e6d
nzhridoy <nayeemuzaman05@gmail.com>
2022-02-20 15:31:10 +0600

    Add data with seeder

---
+++ database/seeders/PostTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $faker = Faker::create();
+
+        foreach(range(1,300) as $index){
+            DB::table('posts')->insert([
+                'title' => $faker->sentence(5),
+                'content' => $faker->paragraph(4),
+            ]);
+        }
+
+    }
+}
